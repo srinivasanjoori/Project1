98871e4e1b84cbcf091eda555edef12e366247de
srinivasanjoori <104360407+srinivasanjoori@users.noreply.github.com>
2023-01-21 05:43:09 +0530

    Create vpc.tf

---
+++ vpc.tf
@@ -0,0 +1,33 @@
+resource "aws_vpc" "main" {
+  cidr_block = "10.0.0.0/16"
+}
+
+resource "aws_subnet" "a" {
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = "10.0.1.0/24"
+  availability_zone = "us-west-2a"
+}
+
+resource "aws_subnet" "b" {
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = "10.0.2.0/24"
+  availability_zone = "us-west-2b"
+}
+
+resource "aws_subnet" "c" {
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = "10.0.3.0/24"
+  availability_zone = "us-west-2c"
+}
+
+resource "aws_security_group" "mongo_sg" {
+  name        = "mongo_sg"
+  description = "Allow MongoDB traffic"
+
+  ingress {
+    from_port   = 27017
+    to_port     = 27017
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
